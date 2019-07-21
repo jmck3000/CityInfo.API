7d3131499c6311c47911659de968ff477e510bee
jmck3000 <jmck3000@gmail.com>
2019-07-21 19:00:29 -0400

    Fist commit.

---
+++ CitiesDataStrore.cs
@@ -0,0 +1,86 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStrore
+    {
+        public static CitiesDataStrore Current { get; } = new CitiesDataStrore();
+        public List<CityDetail> Cities { get; set; }
+
+        public CitiesDataStrore()
+        {
+            Cities = new List<CityDetail>()
+            {
+                new CityDetail()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with the big park.",
+                    PointsOfInterest = new List<PointOfInterestDetail>()
+                    {
+                        new PointOfInterestDetail()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States."
+                        },
+                        new PointOfInterestDetail()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan."
+                        }
+                    }
+                    
+                },
+                 new CityDetail()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral that was never really finished.",
+                    PointsOfInterest = new List<PointOfInterestDetail>()
+                    {
+                        new PointOfInterestDetail()
+                        {
+                            Id = 3,
+                            Name = "Cathedral of Our Lady",
+                            Description = "A Gothic style cathedral, conceived by architects Jan and Pieter Appelmans."
+                        },
+                        new PointOfInterestDetail()
+                        {
+                            Id = 4,
+                            Name = "Antwerp Central Station",
+                            Description = "The finest example of railway architecture in Belgium."
+                        }
+                    }
+                },
+                  new CityDetail()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with the big tower.",
+                     PointsOfInterest = new List<PointOfInterestDetail>()
+                    {
+                        new PointOfInterestDetail()
+                        {
+                            Id = 5,
+                            Name = "Eiffel Tower",
+                            Description = "A wrought iron lattice tower on the Champ de Mars, named after the enginner Gustave Eiffel."
+                        },
+                        new PointOfInterestDetail()
+                        {
+                            Id = 6,
+                            Name = "The Louvre",
+                            Description = "The world's largest museum."
+                        }
+                    }
+                }
+            };
+        }
+
+    }
+}
---
+++ Controllers/PointsOfInterestController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class PointsOfInterestController : Controller
+    {
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStrore.Current.Cities.FirstOrDefault(citys => citys.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        public IActionResult GetPointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStrore.Current.Cities.FirstOrDefault(citys => citys.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(point => point.Id == id);
+            if(pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+
+
+    }
+}
