2b22e86b387ef101211d06b4b23c8a6cd8cd2f55
jmck3000 <jmck3000@gmail.com>
2019-07-28 09:24:37 -0400

    adding mail service.

---
+++ Services/CloudMailService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Services
+{
+    public class CloudMailService
+    {
+        private string _mailTo = Startup.Configuration["mailSettings:mailToAddress"];
+        private string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
+
+        public void Send(string subject, string message)
+        {
+            //send mail - output to debug window
+            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with CloudMailService.");
+            Debug.WriteLine($"Subject: {subject} ");
+            Debug.WriteLine($"Message: {message} ");
+        }
+
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,34 +9,57 @@ using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
+using NLog.Extensions.Logging;
+
+using Microsoft.Extensions.Configuration;
+using CityInfo.API.Services;
 
 namespace CityInfo.API
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc()
                 .AddMvcOptions(o => o.OutputFormatters.Add(
                     new XmlDataContractSerializerOutputFormatter()));
-            //    .AddJsonOptions(o => 
-            //{
+            //.AddJsonOptions(o => {
             //    if (o.SerializerSettings.ContractResolver != null)
             //    {
             //        var castedResolver = o.SerializerSettings.ContractResolver
-            //        as DefaultContractResolver;
+            //            as DefaultContractResolver;
             //        castedResolver.NamingStrategy = null;
             //    }
             //});
+
+#if DEBUG
+            services.AddTransient<IMailService, LocalMailService>();
+#else
+            services.AddTransient<IMailService, CloudMailService>();
+#endif
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            // No need to add these loggers in ASP.NET Core 2.0: the call to WebHost.CreateDefaultBuilder(args) 
+            // in the Program class takes care of that.
+
             loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+
+            //loggerFactory.AddProvider(new NLog.Extensions.Logging.NLogLoggerProvider());
+            loggerFactory.AddNLog();
 
             if (env.IsDevelopment())
             {
@@ -48,6 +71,7 @@ namespace CityInfo.API
             }
 
             app.UseStatusCodePages();
+
             app.UseMvc();
 
             //app.Run(async (context) =>
